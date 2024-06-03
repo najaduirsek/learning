ea2870f531be19241310b1df648b159ef5dcc312
Nataliia Serdiuk <najaduirsek@gmail.com>
2024-06-03 14:15:16 +0300

    Project task 5

--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -40,9 +40,9 @@ def test_product_qnt_update():
 def test_product_insert():
     db = Database()
     db.insert_product(4, 'печиво', 'солодке', 30)
-    cookies_qnt = db.select_product_qnt_by_id(4)
+    water_qnt = db.select_product_qnt_by_id(4)
 
-    assert cookies_qnt[0][0] == 30
+    assert water_qnt[0][0] == 30
 
 
 @pytest.mark.database
@@ -68,3 +68,49 @@ def test_detailed_orders():
     assert orders[0][1] == 'Sergii'
     assert orders[0][2] == 'солодка вода'
     assert orders[0][3] == 'з цукром'
+   
+
+@pytest.mark.database
+def test_text_data_type_insert():
+    db = Database()
+    db.insert_product(5, 'молоко', 'натуральне згущене', '10')
+    milk_qnt = db.select_product_qnt_by_id(5)
+
+    assert milk_qnt[0][0] == 10
+            
+     
+@pytest.mark.database
+def test_check_user_stepan():
+    db = Database()
+    user = db.get_user_address_by_name('Stepan')
+
+    assert user[0][0] == 'Stepana Bandery str, 2'
+    assert user[0][1] == 'Kyiv'
+    assert user[0][2] == '2055'
+    assert user[0][3] == 'Ukraine'     
+    
+    
+@pytest.mark.database
+def test_real_data_type_insert():
+    db = Database()
+    db.insert_product(7, 'real_product', 'real data type', 3.14)
+    product_qnt = db.select_product_qnt_by_id(7)
+
+    assert product_qnt[0][0] == 3.14  
+    
+    
+@pytest.mark.database
+def test_null_data_type_insert():
+    db = Database()
+    db.insert_product(7, 'null_product', 'null data type', 'NULL')
+    qnt = db.select_product_qnt_by_id(7)
+    
+    assert len(qnt) != 0
+    
+    
+@pytest.mark.database
+def test_order_date():
+    db = Database()
+    order_date = db.get_detailed_orders()
+    
+    assert len(order_date) != '00:00:00'     
